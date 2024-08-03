1f05677788397ad638b1de99d11caa6cabe848ed
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-03 12:45:25 +0630

    -

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud_mvc_patter_with_fake_store_api/view/product_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProductList(),
+    );
+  }
+}
