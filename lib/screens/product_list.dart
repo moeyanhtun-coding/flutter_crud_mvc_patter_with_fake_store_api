53f16a752dfe131d8edee0e7af3fd5b9a0764815
moeyanhtun-coding <moeyan227970@gmail.com>
2024-08-03 16:59:02 +0630

    [Add] Product List

--- lib/screens/product_list.dart
+++ lib/screens/product_list.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_mvc_patter_with_fake_store_api/controllers/product_controller.dart';
+import 'package:flutter_crud_mvc_patter_with_fake_store_api/widgets/product_card.dart';
+import 'package:get/get.dart';
+
+import '../models/product_model.dart';
 
 class ProductList extends StatefulWidget {
   const ProductList({super.key});
@@ -8,8 +13,50 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+  final ProductController productController = Get.put(ProductController());
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Products"),
+      ),
+      body: Obx(
+        () {
+          if (productController.isLoading.value) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: productController.products.length,
+              itemBuilder: (context, index) {
+                ProductModel product = productController.products[index];
+                return Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.sizeOf(context).width * 0.5,
+                    child: ProductCard(
+                      imageUrl: '${product.image}',
+                      category: '${product.category}',
+                      price: product.price!,
+                      title: '${product.title}',
+                      onDelete: () {},
+                      onEdit: () {},
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // Navigate to a form to create a new product
+        },
+      ),
+    );
   }
 }
