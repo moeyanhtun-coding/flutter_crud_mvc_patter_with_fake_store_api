f4f19bcc1e93277d4cdc1833a834121a48cbdd28
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-03 12:48:52 +0630

    [Add] rating model

---
+++ lib/models/rating.dart
@@ -0,0 +1,29 @@
+import 'package:collection/collection.dart';
+
+class Rating {
+  double? rate;
+  int? count;
+
+  Rating({this.rate, this.count});
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: (json['rate'] as num?)?.toDouble(),
+        count: json['count'] as int?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'rate': rate,
+        'count': count,
+      };
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    if (other is! Rating) return false;
+    final mapEquals = const DeepCollectionEquality().equals;
+    return mapEquals(other.toJson(), toJson());
+  }
+
+  @override
+  int get hashCode => rate.hashCode ^ count.hashCode;
+}
