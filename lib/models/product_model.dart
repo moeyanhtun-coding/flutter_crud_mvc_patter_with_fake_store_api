e10091075782e1d8858e739293c76d7cf0cc0c8f
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-03 12:45:56 +0630

    [Add] product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,63 @@
+import 'package:collection/collection.dart';
+
+import 'rating.dart';
+
+class ProductModel {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  ProductModel({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json['id'] as int?,
+        title: json['title'] as String?,
+        price: (json['price'] as num?)?.toDouble(),
+        description: json['description'] as String?,
+        category: json['category'] as String?,
+        image: json['image'] as String?,
+        rating: json['rating'] == null
+            ? null
+            : Rating.fromJson(json['rating'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'price': price,
+        'description': description,
+        'category': category,
+        'image': image,
+        'rating': rating?.toJson(),
+      };
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    if (other is! ProductModel) return false;
+    final mapEquals = const DeepCollectionEquality().equals;
+    return mapEquals(other.toJson(), toJson());
+  }
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      title.hashCode ^
+      price.hashCode ^
+      description.hashCode ^
+      category.hashCode ^
+      image.hashCode ^
+      rating.hashCode;
+}
