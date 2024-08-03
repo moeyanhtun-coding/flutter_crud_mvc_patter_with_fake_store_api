ad0bb12b659d0c0badb58f5529bcef6f5dfdd8cd
Moe Yan Htun <moeyan227970@gmail.com>
2024-08-03 12:48:29 +0630

    [Add] Product Service

---
+++ lib/services/product_api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import '../models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class ProductApiService {
+  final String baseUrl = 'https://fakestoreapi.com/products';
+
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    if (response.statusCode == 200) {
+      List<dynamic> body = json.decode(response.body);
+      return body.map((dynamic item) => ProductModel.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
+  Future<ProductModel> fetchProductDetail(int id) async {
+    final response = await http.get(Uri.parse('$baseUrl/$id'));
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load product');
+    }
+  }
+
+  Future<ProductModel> createProduct(ProductModel product) async {
+    final response = await http.post(Uri.parse(baseUrl), headers: {"Content-Type": "application/json"}, body: json.encode(product.toJson()));
+    if (response.statusCode == 200) {
+      return 
+    }
+  }
+}
