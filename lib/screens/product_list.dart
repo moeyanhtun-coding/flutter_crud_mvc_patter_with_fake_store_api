2efdcd2188872d32792b51649e8f8d8d271141d5
moeyanhtun-coding <moeyan227970@gmail.com>
2024-08-04 18:45:30 +0630

    [Add] Show toast

--- lib/screens/product_list.dart
+++ lib/screens/product_list.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
 import 'package:confirm_dialog/confirm_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_mvc_patter_with_fake_store_api/controllers/product_controller.dart';
 import 'package:flutter_crud_mvc_patter_with_fake_store_api/widgets/product_card.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 import '../models/product_model.dart';
@@ -15,6 +18,7 @@ class ProductList extends StatefulWidget {
 
 class _ProductListState extends State<ProductList> {
   final ProductController productController = Get.put(ProductController());
+  late bool isDelete;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,25 +41,34 @@ class _ProductListState extends State<ProductList> {
                   child: SizedBox(
                     width: double.infinity,
                     height: MediaQuery.sizeOf(context).width * 0.5,
-                    child: ProductCard(
-                      imageUrl: '${product.image}',
-                      category: '${product.category}',
-                      price: product.price!,
-                      title: '${product.title}',
-                      onDelete: () async {
-                        if (await confirm(
-                          context,
-                          title: const Text('Confirm'),
-                          content: const Text('Would you like to remove?'),
-                          textOK: const Text('Yes'),
-                          textCancel: const Text('No'),
-                        )) {
-                          productController.deleteProduct(product.id!);
-                          return print('pressedOK');
-                        }
-                        return print('pressedCancel');
+                    child: InkWell(
+                      child: ProductCard(
+                        imageUrl: product.image!,
+                        category: product.category!,
+                        price: product.price!,
+                        title: product.title!,
+                        onDelete: () async {
+                          if (await confirm(
+                            context,
+                            title: const Text('Confirm'),
+                            content: const Text('Would you like to remove?'),
+                            textOK: const Text('Yes'),
+                            textCancel: const Text('No'),
+                          )) {
+                            isDelete = await productController
+                                .deleteProduct(product.id!);
+                            if (isDelete) {
+                              showToast("Deleting Successful", Colors.black,
+                                  Colors.greenAccent);
+                            }
+                          }
+                        },
+                        onEdit: () {},
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/detail',
+                            arguments: product);
                       },
-                      onEdit: () {},
                     ),
                   ),
                 );
@@ -71,5 +84,16 @@ class _ProductListState extends State<ProductList> {
         },
       ),
     );
+  }
+
+  void showToast(String message, Color textColor, backgroundColor) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: backgroundColor,
+        textColor: textColor,
+        fontSize: 16.0);
   }
 }
